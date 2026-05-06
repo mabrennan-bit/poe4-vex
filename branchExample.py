e41426fa2ff091a41fd0113d6191636297b0e722
mabrennan-bit <mabrennan@ctemc.org>
2026-05-05 22:05:40 -0400

    Create branchExample.py

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name : branchExample.py
+Coded by: Mallory Brennan and Dan Lutz
+Description: Demonstrate how to use GitHub branches
+Date: 5/4/26
+"""
+
+counter = 0;
+
+while(counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
